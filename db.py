68bf93e0988e8504c644f024dbcae3590be23e88
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-02-14 13:36:24 +0200

    adding DeclarativeBase

--- db.py
+++ db.py
@@ -1,4 +1,20 @@
-from sqlalchemy.ext.asyncio import create_async_engine
-create_async_engine(
-    "sq;"
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+
+engine = create_async_engine(
+    "sqlite+aiosqlite:///tasks.db"
 )
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+class Model(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+class TaskORM(Model):
